51df61777a11324776401e4f84098f9fc5fef928
codeoflinz <h@hhh.im>
2019-12-01 01:45:13 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaspberryPI_SPI_RC522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
